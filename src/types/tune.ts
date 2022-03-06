78f01ffc3c0fb2ba95ebbdb6fd51b13b48ed3fa5
Piotr Rogowski <piotr.rogowski0@gmail.com>
2022-03-06 21:19:15 +0100

    Add `TuneWithDetails`, update dependencies (#46)

--- src/types/tune.ts
+++ src/types/tune.ts
@@ -10,3 +10,15 @@ export interface TuneConstants {
 export interface Tune {
   constants: TuneConstants;
 }
+
+export interface TuneWithDetails extends Tune {
+  details: {
+    author: string;
+    tuneComment: string;
+    writeDate: string;
+    fileFormat: string;
+    firmwareInfo: string;
+    nPages: number;
+    signature: string;
+  };
+}
